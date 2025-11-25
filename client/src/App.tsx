c3b071176ea7cbae75721c29ffe6a88174f54fea
nathashabrito <britonathasha@gmail.com>
2025-11-25 09:35:46 -0300

    alteracao no default theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
   return (
     <ErrorBoundary>
       <ThemeProvider
-        defaultTheme="dark"
+        defaultTheme="light"
       >
         <TooltipProvider>
           <Toaster />
